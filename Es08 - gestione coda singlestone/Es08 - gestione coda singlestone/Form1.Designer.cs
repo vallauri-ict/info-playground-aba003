2d8f0695a78cd3d17e096c23b9167671b343689b
aba003 <59926534+aba003@users.noreply.github.com>
2020-11-03 17:30:55 +0100

    Add files via upload

---
+++ Es08 - gestione coda singlestone/Es08 - gestione coda singlestone/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Es08___gestione_coda_singlestone
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtLibro = new System.Windows.Forms.TextBox();
+            this.btnPush = new System.Windows.Forms.Button();
+            this.btnPull = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(157, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Inserisci il titolo del libro";
+            // 
+            // txtLibro
+            // 
+            this.txtLibro.Location = new System.Drawing.Point(199, 28);
+            this.txtLibro.Name = "txtLibro";
+            this.txtLibro.Size = new System.Drawing.Size(100, 22);
+            this.txtLibro.TabIndex = 1;
+            // 
+            // btnPush
+            // 
+            this.btnPush.Location = new System.Drawing.Point(348, 19);
+            this.btnPush.Name = "btnPush";
+            this.btnPush.Size = new System.Drawing.Size(133, 35);
+            this.btnPush.TabIndex = 2;
+            this.btnPush.Text = "Aggiungi in coda";
+            this.btnPush.UseVisualStyleBackColor = true;
+            this.btnPush.Click += new System.EventHandler(this.btnPush_Click);
+            // 
+            // btnPull
+            // 
+            this.btnPull.Location = new System.Drawing.Point(141, 105);
+            this.btnPull.Name = "btnPull";
+            this.btnPull.Size = new System.Drawing.Size(249, 44);
+            this.btnPull.TabIndex = 3;
+            this.btnPull.Text = "Leggi il primo della coda";
+            this.btnPull.UseVisualStyleBackColor = true;
+            this.btnPull.Click += new System.EventHandler(this.btnPull_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(493, 190);
+            this.Controls.Add(this.btnPull);
+            this.Controls.Add(this.btnPush);
+            this.Controls.Add(this.txtLibro);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtLibro;
+        private System.Windows.Forms.Button btnPush;
+        private System.Windows.Forms.Button btnPull;
+    }
+}
+
---
+++ Es08 - gestione coda singlestone/Es08 - gestione coda singlestone/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Es08___gestione_coda_singlestone
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPush_Click(object sender, EventArgs e)
+        {
+            if (txtLibro.Text=="")
+            {
+                MessageBox.Show("Inserire il titolo di un libro!");
+            }
+            else
+            {
+                Singleston s = Singleston.GetClsSingleton();
+                s.push(txtLibro.Text);
+                txtLibro.Text = "";
+            }
+        }
+
+        private void btnPull_Click(object sender, EventArgs e)
+        {
+            Singleston s = Singleston.GetClsSingleton();
+            s.pull();
+        }
+    }
+}
