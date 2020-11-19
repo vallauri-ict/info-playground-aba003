765a0f2f73ebf48137c55862e0be7e1b16e8e7c0
aba003 <59926534+aba003@users.noreply.github.com>
2020-11-19 16:14:43 +0100

    Add files via upload

---
+++ Es13 - Gestione Dictionary/13-NegozioDictionary/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace _13_NegozioDictionary
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtKey = new System.Windows.Forms.TextBox();
+            this.txtValue = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnInserisci = new System.Windows.Forms.Button();
+            this.btnRicerca = new System.Windows.Forms.Button();
+            this.btnVisualizza = new System.Windows.Forms.Button();
+            this.lblNum = new System.Windows.Forms.Label();
+            this.lblArt = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtKey
+            // 
+            this.txtKey.Location = new System.Drawing.Point(92, 37);
+            this.txtKey.Name = "txtKey";
+            this.txtKey.Size = new System.Drawing.Size(100, 20);
+            this.txtKey.TabIndex = 0;
+            // 
+            // txtValue
+            // 
+            this.txtValue.Location = new System.Drawing.Point(92, 76);
+            this.txtValue.Name = "txtValue";
+            this.txtValue.Size = new System.Drawing.Size(100, 20);
+            this.txtValue.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Key:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Value:";
+            // 
+            // btnInserisci
+            // 
+            this.btnInserisci.Location = new System.Drawing.Point(12, 145);
+            this.btnInserisci.Name = "btnInserisci";
+            this.btnInserisci.Size = new System.Drawing.Size(83, 30);
+            this.btnInserisci.TabIndex = 4;
+            this.btnInserisci.Text = "Inserisci";
+            this.btnInserisci.UseVisualStyleBackColor = true;
+            this.btnInserisci.Click += new System.EventHandler(this.btnInserisci_Click);
+            // 
+            // btnRicerca
+            // 
+            this.btnRicerca.Location = new System.Drawing.Point(248, 145);
+            this.btnRicerca.Name = "btnRicerca";
+            this.btnRicerca.Size = new System.Drawing.Size(83, 30);
+            this.btnRicerca.TabIndex = 5;
+            this.btnRicerca.Text = "Ricerca";
+            this.btnRicerca.UseVisualStyleBackColor = true;
+            this.btnRicerca.Click += new System.EventHandler(this.btnRicerca_Click);
+            // 
+            // btnVisualizza
+            // 
+            this.btnVisualizza.Location = new System.Drawing.Point(130, 145);
+            this.btnVisualizza.Name = "btnVisualizza";
+            this.btnVisualizza.Size = new System.Drawing.Size(83, 30);
+            this.btnVisualizza.TabIndex = 6;
+            this.btnVisualizza.Text = "Visualizza";
+            this.btnVisualizza.UseVisualStyleBackColor = true;
+            this.btnVisualizza.Click += new System.EventHandler(this.btnVisualizza_Click);
+            // 
+            // lblNum
+            // 
+            this.lblNum.AutoSize = true;
+            this.lblNum.Location = new System.Drawing.Point(224, 44);
+            this.lblNum.Name = "lblNum";
+            this.lblNum.Size = new System.Drawing.Size(98, 13);
+            this.lblNum.TabIndex = 7;
+            this.lblNum.Text = "Numero elementi: 0";
+            // 
+            // lblArt
+            // 
+            this.lblArt.AutoSize = true;
+            this.lblArt.Location = new System.Drawing.Point(224, 83);
+            this.lblArt.Name = "lblArt";
+            this.lblArt.Size = new System.Drawing.Size(194, 13);
+            this.lblArt.TabIndex = 8;
+            this.lblArt.Text = "Nome articolo cercato: nessuna articolo";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(429, 209);
+            this.Controls.Add(this.lblArt);
+            this.Controls.Add(this.lblNum);
+            this.Controls.Add(this.btnVisualizza);
+            this.Controls.Add(this.btnRicerca);
+            this.Controls.Add(this.btnInserisci);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtValue);
+            this.Controls.Add(this.txtKey);
+            this.Name = "Form1";
+            this.Text = "Negozio Articoli con Dictionary";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtKey;
+        private System.Windows.Forms.TextBox txtValue;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnInserisci;
+        private System.Windows.Forms.Button btnRicerca;
+        private System.Windows.Forms.Button btnVisualizza;
+        private System.Windows.Forms.Label lblNum;
+        private System.Windows.Forms.Label lblArt;
+    }
+}
+
---
+++ Es13 - Gestione Dictionary/13-NegozioDictionary/Form1.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _13_NegozioDictionary
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public struct book
+        {
+            public int key;
+            public string value;
+        }
+        Dictionary<int, book> diz = new Dictionary<int, book>();
+        //int i = 0;
+        private void btnInserisci_Click(object sender, EventArgs e)
+        {
+            book b;
+            b.key = Convert.ToInt32(txtKey.Text);
+            b.value = txtValue.Text;
+            diz.Add(Convert.ToInt32(txtKey.Text), b);
+            MessageBox.Show("Inserimento effettuato correttamente");
+        }
+
+        private void btnVisualizza_Click(object sender, EventArgs e)
+        {
+            lblNum.Text = "Numero elementi: " + diz.Count;
+        }
+
+        private void btnRicerca_Click(object sender, EventArgs e)
+        {
+            int k = Convert.ToInt32(Interaction.InputBox("Inserisci la chiave dell'articolo da ricercare: ","Articolo da cercare"));
+            lblArt.Text = "Nome articolo cercato: " + diz[k].value;
+        }
+    }
+}
