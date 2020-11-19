095b0e1f9b64c7144248cecf19fbd3a97fd98e0a
aba003 <59926534+aba003@users.noreply.github.com>
2020-11-19 17:08:54 +0100

    Add files via upload

---
+++ Es14-Pronto soccorso/Es14-Pronto soccorso/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace Es14_Pronto_soccorso
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cmbCodice = new System.Windows.Forms.ComboBox();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtTemp = new System.Windows.Forms.TextBox();
+            this.btnInserisci = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txteta = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nome ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(382, 44);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 17);
+            this.label3.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(24, 165);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(90, 17);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Temperatura";
+            // 
+            // cmbCodice
+            // 
+            this.cmbCodice.FormattingEnabled = true;
+            this.cmbCodice.Items.AddRange(new object[] {
+            "Bianco",
+            "Verde",
+            "Giallo",
+            "Rosso"});
+            this.cmbCodice.Location = new System.Drawing.Point(120, 112);
+            this.cmbCodice.Name = "cmbCodice";
+            this.cmbCodice.Size = new System.Drawing.Size(121, 24);
+            this.cmbCodice.TabIndex = 4;
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(120, 45);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(100, 22);
+            this.txtNome.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(24, 115);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(51, 17);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Codice";
+            // 
+            // txtTemp
+            // 
+            this.txtTemp.Location = new System.Drawing.Point(120, 165);
+            this.txtTemp.Name = "txtTemp";
+            this.txtTemp.Size = new System.Drawing.Size(100, 22);
+            this.txtTemp.TabIndex = 8;
+            // 
+            // btnInserisci
+            // 
+            this.btnInserisci.Location = new System.Drawing.Point(27, 226);
+            this.btnInserisci.Name = "btnInserisci";
+            this.btnInserisci.Size = new System.Drawing.Size(144, 35);
+            this.btnInserisci.TabIndex = 9;
+            this.btnInserisci.Text = "Inserisci paziente";
+            this.btnInserisci.UseVisualStyleBackColor = true;
+            this.btnInserisci.Click += new System.EventHandler(this.btnInserisci_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(226, 226);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(131, 35);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Cura paziente";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txteta
+            // 
+            this.txteta.Location = new System.Drawing.Point(120, 79);
+            this.txteta.Name = "txteta";
+            this.txteta.Size = new System.Drawing.Size(100, 22);
+            this.txteta.TabIndex = 12;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 17);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Età";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(385, 226);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(128, 35);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Temperatura";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(559, 320);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.txteta);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnInserisci);
+            this.Controls.Add(this.txtTemp);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.cmbCodice);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cmbCodice;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtTemp;
+        private System.Windows.Forms.Button btnInserisci;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txteta;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Es14-Pronto soccorso/Es14-Pronto soccorso/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Es14_Pronto_soccorso
+{
+    public partial class Form1 : Form
+    {
+        public struct Paziente
+        {
+            public string nome;
+            public string eta;
+            public string temperatura;
+            public string colore;
+        }
+        Queue<string> CodiceBianco = new Queue<string>();
+        Queue<string> CodiceVerde = new Queue<string>();
+        Queue<string> CodiceGiallo = new Queue<string>();
+        Queue<string> CodiceRosso = new Queue<string>();
+        int temperatura = 0;
+        string nome = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnInserisci_Click(object sender, EventArgs e)
+        {
+            Paziente paziente;
+            paziente.nome = txtNome.Text;
+            paziente.eta = txteta.Text;
+            paziente.temperatura = txtTemp.Text;
+            paziente.colore = cmbCodice.Text;
+            if (Convert.ToInt32(paziente.temperatura)>=temperatura)
+            {
+                temperatura = Convert.ToInt32(paziente.temperatura);
+                nome = paziente.nome;
+            }
+            switch (paziente.colore)
+            {
+                case "Rosso":
+                    CodiceRosso.Enqueue(paziente.nome);
+                    break;
+                case "Giallo":
+                    CodiceGiallo.Enqueue(paziente.nome);
+                    break;
+                case "Verde":
+                    CodiceVerde.Enqueue(paziente.nome);
+                    break;
+                case "Bianco":
+                    CodiceBianco.Enqueue(paziente.nome);
+                    break;
+                default:
+                    break;
+            }
+            txtNome.Text = "";
+            txteta.Text = "";
+            txtTemp.Text = "";
+            cmbCodice.SelectedIndex = -1;
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (CodiceRosso.Count!=0)
+            {
+                MessageBox.Show(CodiceRosso.Dequeue());
+            }
+            else if (CodiceGiallo.Count != 0)
+            {
+                MessageBox.Show(CodiceGiallo.Dequeue());
+            }
+            else if (CodiceVerde.Count != 0)
+            {
+                MessageBox.Show(CodiceVerde.Dequeue());
+            }
+            else
+            {
+                MessageBox.Show(CodiceBianco.Dequeue());
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("La persona con la temperatura piu alta è: " + nome + " e la sua temperatura è stata: " + temperatura);
+        }
+    }
+}
