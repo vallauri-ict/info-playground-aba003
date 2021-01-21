8766de295fe367ccfe9937d427eb980452a1a642
Ababei Edoardo <e.ababei.1257@vallauri.edu>
2021-01-21 09:53:41 +0100

    Aggiunti esercizi mancanti

---
+++ Es22-Scopa/22-Scopa/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace _22_Scopa
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEstrai = new System.Windows.Forms.Button();
+            this.btnMazzo = new System.Windows.Forms.Button();
+            this.btnMostraMazzo = new System.Windows.Forms.Button();
+            this.btnMescolaMazzo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnEstrai
+            // 
+            this.btnEstrai.Location = new System.Drawing.Point(12, 12);
+            this.btnEstrai.Name = "btnEstrai";
+            this.btnEstrai.Size = new System.Drawing.Size(157, 99);
+            this.btnEstrai.TabIndex = 0;
+            this.btnEstrai.Text = "Estrai";
+            this.btnEstrai.UseVisualStyleBackColor = true;
+            this.btnEstrai.Click += new System.EventHandler(this.btnEstrai_Click);
+            // 
+            // btnMazzo
+            // 
+            this.btnMazzo.Location = new System.Drawing.Point(175, 12);
+            this.btnMazzo.Name = "btnMazzo";
+            this.btnMazzo.Size = new System.Drawing.Size(157, 99);
+            this.btnMazzo.TabIndex = 1;
+            this.btnMazzo.Text = "Mazzo";
+            this.btnMazzo.UseVisualStyleBackColor = true;
+            this.btnMazzo.Click += new System.EventHandler(this.btnMazzo_Click);
+            // 
+            // btnMostraMazzo
+            // 
+            this.btnMostraMazzo.Location = new System.Drawing.Point(12, 117);
+            this.btnMostraMazzo.Name = "btnMostraMazzo";
+            this.btnMostraMazzo.Size = new System.Drawing.Size(157, 99);
+            this.btnMostraMazzo.TabIndex = 2;
+            this.btnMostraMazzo.Text = "Mostra mazzo";
+            this.btnMostraMazzo.UseVisualStyleBackColor = true;
+            this.btnMostraMazzo.Click += new System.EventHandler(this.btnMostraMazzo_Click);
+            // 
+            // btnMescolaMazzo
+            // 
+            this.btnMescolaMazzo.Location = new System.Drawing.Point(175, 117);
+            this.btnMescolaMazzo.Name = "btnMescolaMazzo";
+            this.btnMescolaMazzo.Size = new System.Drawing.Size(157, 99);
+            this.btnMescolaMazzo.TabIndex = 3;
+            this.btnMescolaMazzo.Text = "Mescola mazzo";
+            this.btnMescolaMazzo.UseVisualStyleBackColor = true;
+            this.btnMescolaMazzo.Click += new System.EventHandler(this.btnMescolaMazzo_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(344, 228);
+            this.Controls.Add(this.btnMescolaMazzo);
+            this.Controls.Add(this.btnMostraMazzo);
+            this.Controls.Add(this.btnMazzo);
+            this.Controls.Add(this.btnEstrai);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEstrai;
+        private System.Windows.Forms.Button btnMazzo;
+        private System.Windows.Forms.Button btnMostraMazzo;
+        private System.Windows.Forms.Button btnMescolaMazzo;
+    }
+}
+
---
+++ Es22-Scopa/22-Scopa/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _22_Scopa
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Mazzo mazzo;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Mano mano = new Mano();
+        }
+
+        private void btnEstrai_Click(object sender, EventArgs e)
+        {
+            Carta estratta;
+
+            estratta = mazzo.Estrai();
+            MessageBox.Show("Carta estratta: " + estratta.Valore + " di " + estratta.Seme);
+        }
+
+        private void btnMostraMazzo_Click(object sender, EventArgs e)
+        {
+            foreach (var carta in mazzo.mazzo)
+            {
+                MessageBox.Show(carta.Valore + " di " + carta.Seme);
+            }
+        }
+
+        private void btnMazzo_Click(object sender, EventArgs e)
+        {
+            mazzo = new Mazzo();
+            MessageBox.Show("Creato nuovo mazzo");
+        }
+
+        private void btnMescolaMazzo_Click(object sender, EventArgs e)
+        {
+            mazzo.Mescola();
+        }
+    }
+}
