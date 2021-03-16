f3851bb4c74030af90622e9438c519c672819674
Ababei Edoardo <e.ababei.1257@vallauri.edu>
2021-03-16 22:13:43 +0100

    Aggiutno esercizio 26 interfacce

---
+++ Es26-Interfacce/26-EsInterfacce/MyComponent.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _26_EsInterfacce
+{
+    class MyComponent : IDispAggiuntivo
+    {
+        private int _id = 0;
+        private bool _status;
+        public int id { get => _id; set => _id = value; }
+
+        public bool status => _status;
+
+        public void connetti(string s)
+        {
+            _status = true;
+            id++;
+            System.Windows.Forms.MessageBox.Show("Connesso a: " + s);
+        }
+
+        public void disconnetti(string s)
+        {
+            _status = false;
+            System.Windows.Forms.MessageBox.Show("Disconnesso da: " + s);
+        }
+
+        public void Stato()
+        {
+            if (status)
+            {
+                System.Windows.Forms.MessageBox.Show("Attualmente Connesso con id: " + id);
+            }
+            else
+                System.Windows.Forms.MessageBox.Show("Non Connesso!");
+        }
+    }
+}
---
+++ Es26-Interfacce/README.md
@@ -0,0 +1,15 @@
+# info-playground-MarengoSimone
+
+_Stud. Marengo Simone_
+
+## Progetto 26
+## Obiettivo: Simulare una connessione tramite un id con le interfacce
+
+## L'interfaccia IDispAggiuntivo:
+- Stabilisce quale aspetto dovrà avere un oggetto. In particolare presenta un campo intero **id** e un boolean **status** per gestire la connessione
+- Presenta anche due metodi connetti e disconneti ai quali va passata una stringa (nome della connessione)
+
+## La classe MyComponents:
+- Implementa tramite delle get e set l'id e lo stato della connessione
+- Implementa i metodi connetti e disconnetti. Essi tramite una message box restituiscono se si è connessi oppure no alla connessione passata come parametro nel Form1
+- Implementa un metodo Stato() che restituisce lo stato corrente della connessione
