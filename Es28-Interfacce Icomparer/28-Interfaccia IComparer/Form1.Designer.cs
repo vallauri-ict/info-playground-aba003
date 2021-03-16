bbcd163de67dfe039dee6f4ce072a154e3c15861
Ababei Edoardo <e.ababei.1257@vallauri.edu>
2021-03-16 22:15:23 +0100

    Aggiunto es 28 interfacce icomparer

---
+++ Es28-Interfacce Icomparer/28-Interfaccia IComparer/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace _28_Interfaccia_IComparer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCognome = new System.Windows.Forms.Button();
+            this.btnNome = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnCognome
+            // 
+            this.btnCognome.Location = new System.Drawing.Point(12, 22);
+            this.btnCognome.Name = "btnCognome";
+            this.btnCognome.Size = new System.Drawing.Size(143, 30);
+            this.btnCognome.TabIndex = 1;
+            this.btnCognome.Text = "Ordina per Cognome";
+            this.btnCognome.UseVisualStyleBackColor = true;
+            this.btnCognome.Click += new System.EventHandler(this.btnCognome_Click);
+            // 
+            // btnNome
+            // 
+            this.btnNome.Location = new System.Drawing.Point(193, 22);
+            this.btnNome.Name = "btnNome";
+            this.btnNome.Size = new System.Drawing.Size(143, 30);
+            this.btnNome.TabIndex = 2;
+            this.btnNome.Text = "Ordina per Nome";
+            this.btnNome.UseVisualStyleBackColor = true;
+            this.btnNome.Click += new System.EventHandler(this.btnNome_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(370, 87);
+            this.Controls.Add(this.btnNome);
+            this.Controls.Add(this.btnCognome);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCognome;
+        private System.Windows.Forms.Button btnNome;
+    }
+}
+
---
+++ Es28-Interfacce Icomparer/README.md
@@ -0,0 +1,6 @@
+# info-playground-MarengoSimone
+
+_Stud. Marengo Simone_
+
+## Progetto 28:
+Primo esercizio con l'utilizzo dell'**interfaccia IComparer**
